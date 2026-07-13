1124f637ef57f9ece6b395cedd68e3c66744bc70
Vignesh <vignesh24cs@rnsit.ac.in>
2026-07-13 19:31:28 +0530

    Clean up whitespace in jwt.js
    Remove unnecessary whitespace in jwt.js

--- jwt.js
+++ jwt.js
@@ -13,8 +13,6 @@ const jwtAuthMiddleware = (req, res, next) => {
     try{
         
         const decoded = jwt.verify(token, process.env.JWT_SECRET);
-
-        
         req.user = decoded
         next();
     }catch(err){
