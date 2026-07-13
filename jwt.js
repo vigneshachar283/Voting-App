feacc73f9b26621cf1f3da364f29c8a544629548
Vignesh <vignesh24cs@rnsit.ac.in>
2026-07-13 19:31:53 +0530

    Remove unnecessary blank line in jwt.js

--- jwt.js
+++ jwt.js
@@ -1,7 +1,6 @@
 const jwt = require('jsonwebtoken');
 
 const jwtAuthMiddleware = (req, res, next) => {
-
     
     const authorization = req.headers.authorization
     if(!authorization) return res.status(401).json({ error: 'Token Not Found' });
